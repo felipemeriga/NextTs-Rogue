76329b188c5262c49183d38e56a8e6f7774d6515
felipemeriga <felipe.meriga@gmail.com>
2021-04-06 10:56:42 -0300

    Eliminating getStaticProps from _app.tsx, also queryCache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,12 @@
 // pages/_app.js
-import React from 'react'
 import App from 'next/app'
 import '../styles.css'
-import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { Hydrate } from 'react-query/hydration'
-import { NextPageContext } from 'next'
 
 export const queryClient = new QueryClient()
 
-export const queryCache = new QueryCache({
-    onError: (error) => {
-        console.log(error)
-    },
-})
-
-interface Context extends NextPageContext {
-    // any modifications to the default context, e.g. query types
-}
-
-interface IProps {
-    ctx: Context
-    Component: any
-}
-
 class MyApp extends App {
-    // @ts-ignore
-    static async getInitialProps({ Component, ctx }: IProps): Promise<any> {
-        return {
-            pageProps: {
-                // Call page-level getInitialProps
-                ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-            },
-        }
-    }
 
     render(): JSX.Element {
         const { Component, pageProps } = this.props
